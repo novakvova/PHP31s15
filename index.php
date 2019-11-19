e37812b056787960b5e657d7f4407893f0a6b3ca
novakvova <novakvova@gmail.com>
2019-11-19 15:44:05 +0200

    Begin work project

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php include "_header.php"; ?>
+
+    <h1>Home Page</h1>
+    <?php
+        $a=23;
+        $b=18;
+        $c=$a+$b;
+        echo "<h2>Hello echo</h2>\n";
+    ?>
+    
+<?php include "_footer.php";
